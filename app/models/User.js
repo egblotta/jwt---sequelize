dc17faa83ba54f55436517a1035dd8294086318c
Emiliano Gomez <egomez@labsxd.com>
2021-07-27 11:16:41 -0300

    Refactorizacion de codigo

--- app/models/User.js
+++ app/models/User.js
@@ -4,25 +4,28 @@ const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
 
   class User extends Model {
-    
+
+    //Asociaciones con post y roles
     static associate(models) {      
+
       User.hasMany(models.Post, { as: 'posts', foreignKey: 'userId' });
       User.belongsToMany(models.Role, { as: 'roles', through: 'user_role', foreignKey: 'user_id', });
+
     }
   }
 
+  //Atributos de usuario con validaciones
   User.init(
     {
       name: {
         type: DataTypes.STRING,
         allowNull: false,
-        //Validaciones
         validate: {
           isAlpha: {
             msg: 'El nombre solo puede contener letras',
           },
           len: {
-            args: [3, 20],
+            args: [4, 20],
             msg: 'El nombre debe tener entre 4 y 20 caracteres',
           },
         },
@@ -56,6 +59,7 @@ module.exports = (sequelize, DataTypes) => {
 
   //Comprueba que el usuario es administrador
   User.isAdmin = function (roles) {
+    
     let tmpArray = [];  //vector temporal
 
     roles.forEach(role => tmpArray.push(role.role));
